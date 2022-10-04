51379f8d8856e6c95bab7c7906639858be29613f
thanhngan22 <nganbui9a2017@gmail.com>
2022-10-04 17:49:12 +0700

    :writing_hand: HackerRank: Heap/QHeap1

--- Heap/QHeap1.cpp
+++ Heap/QHeap1.cpp
@@ -9,8 +9,10 @@ int main() {
     int n;
     cin >> n;
     vector <int> Q;
+    stringstream result;
     for (int i = 0; i < n ; i++) {
         string s;
+        fflush(stdin);
         getline(cin, s);
         stringstream ss(s);
         string mem;
@@ -19,12 +21,34 @@ int main() {
                 ss >> mem;
                 Q.push_back(stoi(mem));
             } else if (mem == "2") {
-                
+                ss >> mem;
+                for (int i = 0; i < Q.size(); i++) {
+                    if (Q[i] == stoi(mem)) {
+                        Q.erase(Q.begin() + i);
+                        break;
+                    }
+                }
+            } else if (mem == "3") {
+                int min = Q[0];
+                for (int i = 1; i < Q.size(); i++) {
+                    if (Q[i] < min) {
+                        min = Q[i];
+                    }
+                }
+                for (int i = 0; i < Q.size(); i++) {
+                    if (Q[i] == min) {
+                        result << min << endl;
+                    }
+                }
             }
         }
 
     }
+
+    cout << result.str();
+
+    return 225;
 }
 
 
-// chưa xong
+
