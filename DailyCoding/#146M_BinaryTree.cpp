9bc2561843b7d670caf1a0d203ce07fec3b6d06e
thanhngan22 <nganbui9a2017@gmail.com>
2022-10-06 15:02:21 +0700

    :writing_hand: Daily Coding :#146_Medium / BinaryTree / prune the tree

--- DailyCoding/#146M_BinaryTree.cpp
+++ DailyCoding/#146M_BinaryTree.cpp
@@ -21,5 +21,55 @@ should be pruned to:
 We do not remove the tree at the root or its left child because it still has a 1 as a descendant.
 */
 
+#include <iostream>
+using namespace std;
 
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node (int data) {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void PruneTree(Node *&root) {
+   if (root == NULL) {
+       return;
+   }
+   PruneTree(root->left);
+   PruneTree(root->right);
+
+   if (root->left == NULL && root->right == NULL && root->data == 0) {
+       root = NULL;
+   }
+}
+
+void PrintList (Node *root ) {
+      if (root == NULL) {
+         return;
+      }
+      cout << root->data << " ";
+      PrintList(root->left);
+      PrintList(root->right);
+}
+
+int main() {
+      Node *root = new Node(0);
+      root->left = new Node(1);
+      root->right = new Node(0);
+      root->right->left = new Node(1);
+      root->right->right = new Node(0);
+      root->right->left->left = new Node(0);
+      root->right->left->right = new Node(0);
+   
+      PruneTree(root);
+
+      PrintList(root);
+
+      return 0;
+
+}
 
