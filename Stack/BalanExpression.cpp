9f41899e050a821247c6d17777dae13e402101a5
thanhngan22 <nganbui9a2017@gmail.com>
2022-10-21 09:28:10 +0700

    :cry: working on convert middle exp into postfix exp but get bugs in push an element to top of stack

--- Stack/BalanExpression.cpp
+++ Stack/BalanExpression.cpp
@@ -72,6 +72,29 @@ void enqueue (Queue *&Q, char data) {
     Q->rear = pNode;
 }
 
+// pop an element on top of stack out of stack
+void pop (Stack *S) {
+    Node *temp = S->top;
+    S->top = S->top->pNext;
+    delete (temp);
+}
+
+// push an element to top of stack
+void push (Stack *&S, char key) {
+    if (S->top == NULL) {
+        cout << "key: " << key << endl;
+        Node *pNode = createNode(key);
+        cout << "2245";
+        S->top = pNode;
+        return ;
+    }
+    Node *pNode = new Node;
+    pNode->data = key;
+    pNode->hasKey = false;
+    pNode->pNext = S->top;
+    S->top = pNode;
+}
+
 // check whether a char is number or not
 bool isNumber (char c) {
     if (c >= '0' && c <= '9') {
@@ -167,8 +190,56 @@ Queue *convertExpString_Queue (string exp) {
 }
 
 // convert middle expression into postfix expression
-Queue *convertMidExpIntoPostExp () {
+Queue *convertMidExpIntoPostExp (Queue *P) {
+    Stack *S = new Stack;
+    S->top = NULL;
+    Queue *Q = new Queue;
+    Node *temp = P->front;
+    while (temp != NULL) {
+        if (temp->hasKey == true) {
+            enqueue(Q, temp->key);
+            cout << "enqueue " << temp->key << endl;
+            temp = temp->pNext;
+            continue;
+        } else {
+            cout << "range: " << temp->data << endl;
+            if (temp->data == ')') {
+                while (S->top != NULL) {
+                        if (S->top->data == '(') {
+                            pop(S);
+                            temp = temp->pNext;
+                            break;
+                        } else {
+                            enqueue(Q, S->top->data);
+                            pop(S);
+                        }
+                }
+            }
+            if (temp->data == '(') {
+                push(S, temp->data);
+                temp = temp->pNext;
+                continue;
+            }
+            if (isOperator(temp->data)) {
+                if (S->top == NULL || ! isOperator(S->top->data) || isMorePriority(temp->data, S->top->data)) {
+                    push(S, temp->data);
+                    temp = temp->pNext;
+                    cout << "day toan tu vao stack\n";
 
+                    continue;
+                }
+                while (S->top != NULL && isOperator(S->top->data) && isMorePriority(S->top->data, temp->data)) {
+                    enqueue(Q, S->top->data);
+                    pop(S);
+                } 
+                push(S, temp->data);
+                temp = temp->pNext;
+                continue;
+            }
+        }
+    }
+    cout << "heli";
+    return Q;
 }
 
 // calculate the value of postfix expressions
@@ -183,11 +254,14 @@ int main() {
     fflush(stdin);
     getline(cin, exp);
 
-    Queue *Q = convertExpString_Queue(exp);
-    if (Q->front->hasKey == true) {
-        cout << "hi: ";
-        cout << Q->front->key << endl;
-    }
+    Queue *P = convertExpString_Queue(exp);
+    printQueue(P);
+
+    // convert middle expression into postfix expression
+    cout << "Middle expression: " ;
+    printQueue(P);
+    cout << "Postfix expression: ";
+    Queue *Q = convertMidExpIntoPostExp(P);
     printQueue(Q);
 
     return 225;
