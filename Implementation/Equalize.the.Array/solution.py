29ac4d80ffdd63dfe3d2ed38f75b4880700b854b
thanhngan22 <nganbui9a2017@gmail.com>
2023-03-31 17:00:37 +0700

    Equalize the array | add solution in py

---
+++ Implementation/Equalize.the.Array/solution.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    # Write your code here
+    lenA = len(arr)
+    maxCount = 0;
+    for x in arr:
+        count = arr.count(x)
+        maxCount = count if count > maxCount else maxCount
+        arr = [ i for i in arr if i != x]
+    return lenA - maxCount
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
