0bdadd9fc87735db10b0e4b5e18d65b5160feaeb
thanhngan22 <nganbui9a2017@gmail.com>
2023-04-04 16:48:52 +0700

    Super reduced string | add solution py

---
+++ Strings/Super.Reduced.String/solution.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def superReducedString(s):
+    # Write your code here
+    stack = []
+    for c in s:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else :
+            stack.append(c)
+    res = ''.join(stack)
+    return res if res else 'Empty String'
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
