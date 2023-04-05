85d4d356c73ec45e571ee51531dfb5396c4ebfea
thanhngan22 <nganbui9a2017@gmail.com>
2023-04-05 10:28:06 +0700

    Hackerrank in a string: add solution py

---
+++ Strings/Hackerrank.in.a.String/solution.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hackerrankInString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def hackerrankInString(s):
+    # Write your code here
+    text = ['h','a','c','k','e','r','r','a','n','k']
+    for c in s:
+        # print(f"c: {c}, text: {text}")
+        if c == text[0]:
+            text.pop(0)
+        if len(text) == 0:
+            return 'YES'
+    return 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = hackerrankInString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
