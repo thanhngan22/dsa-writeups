0687bb2baa6a2a17edb5111a4f02a2af23ddd546
thanhngan22 <nganbui9a2017@gmail.com>
2023-04-05 19:51:55 +0700

    Beautiful Binary String | add solution py

---
+++ Strings/Beautiful.Binary.String/solution.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulBinaryString' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING b as parameter.
+#
+
+def beautifulBinaryString(b):
+    # Write your code here
+    count = 0
+    for i in range(2, len(b)):
+        if b[i] == '0' and b[i-1] == '1' and b[i-2] == '0':
+            b = b[:i] + '1' + b[i+1:]
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    b = input()
+
+    result = beautifulBinaryString(b)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
