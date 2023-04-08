b6e0357dc1f6e946431ce662b9a5bd76df31b8fc
thanhngan22 <nganbui9a2017@gmail.com>
2023-04-08 22:13:28 +0700

    Missing numbers | add solution py

---
+++ Search/Missing.Numbers/solution.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'missingNumbers' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY arr
+#  2. INTEGER_ARRAY brr
+#
+
+def missingNumbers(arr, brr):
+    # Write your code here
+    distinct = set(brr)
+    shortage = []
+    for x in distinct:
+        countA = arr.count(x)
+        countB = brr.count(x)
+        diff = countB - countA
+        if diff > 0:
+            shortage.append(x)
+    return shortage
+                
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    m = int(input().strip())
+
+    brr = list(map(int, input().rstrip().split()))
+
+    result = missingNumbers(arr, brr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
