eba5b3440a18f5868d453d5e1ba5ab44c94bc3f7
thanhngan22 <nganbui9a2017@gmail.com>
2023-04-09 20:54:22 +0700

    Halloween Sale | add solution py

---
+++ Implementation/Halloween.Sale/solution.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'howManyGames' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER p
+#  2. INTEGER d
+#  3. INTEGER m
+#  4. INTEGER s
+#
+
+def howManyGames(p, d, m, s):
+    # Return the number of games you can buy
+    count = 0
+    sum = 0
+    while sum + p <= s :
+        count += 1
+        sum += p
+        p = m if p - d <= m else p - d
+        
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    p = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    m = int(first_multiple_input[2])
+
+    s = int(first_multiple_input[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
