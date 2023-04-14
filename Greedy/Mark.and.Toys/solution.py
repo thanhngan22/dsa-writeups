5d7064cc49e9116389a87bd9765f2cd40524f754
thanhngan22 <nganbui9a2017@gmail.com>
2023-04-14 14:42:20 +0700

    Mark and Toys | add solution py

---
+++ Greedy/Mark.and.Toys/solution.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumToys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER k
+#
+
+def maximumToys(prices, k):
+    # Write your code here
+    prices.sort()
+    count = 0
+    i = 0
+    while k > 0 and prices[i] < k:
+        count += 1
+        k -= prices[i]
+        i += 1
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
